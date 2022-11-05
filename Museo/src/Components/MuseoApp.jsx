c793ca83cbbd932ba02f183a44c896dc6b7641b2
Alejandro <alejandrooramash@gmail.com>
2022-11-05 00:17:08 -0400

    ok

---
+++ Museo/src/Components/MuseoApp.jsx
@@ -0,0 +1,12 @@
+import { Navegador } from "./Navegador";
+import { Carrousel } from "./Carrousel";
+import './index.css';
+
+export const MuseoApp = () => {
+  return (
+	<>
+	  <Navegador />
+	  <Carrousel />
+	</>
+  )
+}
