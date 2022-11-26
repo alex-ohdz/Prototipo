96f48a9f5571399008f398e6d3e8d9e6cdc5ed34
Alejandro Oramas H <99675215+alex-ohdz@users.noreply.github.com>
2022-11-26 16:26:09 -0500

    Revert "Revert "ok""
    This reverts commit 36738fa5c6bfa7efb7c822212d5e9df060cd2545.

--- Museo/src/Components/CollectionPages/ImageSlider.jsx
+++ Museo/src/Components/CollectionPages/ImageSlider.jsx
@@ -1,15 +1,15 @@
-import React, { useState, useRef } from "react";
-import { SliderData } from "../../assets/Data/SliderData";
+import React, { useEffect, useState } from "react";
 import { FaArrowAltCircleRight, FaArrowAltCircleLeft } from "react-icons/fa";
-import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
-import Tooltip from "react-bootstrap/Tooltip";
+import OverlayTrigger from "react-bootstrap/OverlayTrigger";
+import { AccordionSlider } from "./Accordion";
+import { RenderTooltip } from "./PartComponents/RenderTooltip";
+import { SliderData } from "../../assets/Data/SliderData";
+
 
 export const ImageSlider = ({ slides }) => {
-  const [current, setCurrent] = useState(0);
-  const [show, setShow] = useState(false);
-  const target = useRef(null);
-  const length = slides.length;
 
+  const [current, setCurrent] = useState(0);
+  const length = slides.length;
   const nextSlide = () => {
     setCurrent(current === length - 1 ? 0 : current + 1);
   };
@@ -20,11 +20,7 @@ export const ImageSlider = ({ slides }) => {
   if (!Array.isArray(slides) || slides.length <= 0) {
     return null;
   }
-  const renderTooltip = (props) => (
-    <Tooltip id="imgTooltip" {...props}>
-      <span><em>Titulo de la Obra</em>, Autor</span>
-    </Tooltip>
-  );
+
   return (
     <>
       <h2 className="linea">
@@ -36,22 +32,24 @@ export const ImageSlider = ({ slides }) => {
         {SliderData.map((slide, index) => {
           return (
             <OverlayTrigger
-            placement="bottom"
-            delay={{ show: 250, hide: 400 }}
-            overlay={renderTooltip}
-          >
-            <div
-              className={index === current ? "slide active" : "slide"}
               key={index}
+              placement="bottom"
+              delay={{ show: 200, hide: 400 }}
+              overlay={(props) => RenderTooltip({ slide, ...props })}
             >
-              {index === current && (
-                <img src={slide.image} alt="travel image" className="image" />
-              )}
-            </div>
+              <div
+                className={index === current ? "slide active" : "slide"}
+                key={index}
+              >
+                {index === current && (
+                  <img src={slide.image} alt="travel image" className="image" />
+                )}
+              </div>
             </OverlayTrigger>
           );
         })}
       </section>
+      <AccordionSlider slide={slides[current]} />
     </>
   );
 };
