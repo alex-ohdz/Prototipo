fc97b6e26e4969f059ce05394b050626b86f7e15
Alejandro Oramas H <99675215+alex-ohdz@users.noreply.github.com>
2022-12-01 22:54:29 -0500

    prototipo de eventos

--- src/Components/CollectionPages/PartComponents/RenderTooltip.jsx
+++
@@ -1,9 +0,0 @@
-import Tooltip from "react-bootstrap/Tooltip";
-export const RenderTooltip = ({picture}) => {
-	return (
-	<Tooltip id="imgTooltip">
-	  <span><em>{picture.name}</em>,probando {picture.status}</span>
-	</Tooltip>
-	)
-  };
-  //
--- src/Components/EventosPages/PartsComponents/Gallery.jsx
+++ src/Components/EventosPages/PartsComponents/Gallery.jsx
@@ -1,13 +1,36 @@
-import React from 'react'
-import { SliderData } from '../../../assets/Data/SliderData'
-	   
+import React from "react";
+import { SliderData } from "../../../assets/Data/SliderData";
+
 export const Gallery = () => {
+  const renderGallery = (gallery, index) => {
+    return (
+      <>
+        <div className="contSup">
+          <div className="gallery-cont" key={index}>
+            <img
+              className="lazyload  gallery-img img-fluid"
+              src={gallery.image}
+              alt={gallery.alt}
+            />
+            <div className="menuDown">
+              <div className="contadorStar">
+              <p>4</p>
+              </div>
+              <div className="contadorButton">
+                <button className="bStar">
+                  <i class="bi bi-star"></i>
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  };
   return (
-	<>
-	<h1>Galería</h1>
-	<div className='container-fluid'>
-		<img src="https://th.bing.com/th/id/R.cf0706dd6393794a32b1257580398507?rik=8oTH1O%2bPQjPLXg&pid=ImgRaw&r=0" alt="" />	
-	</div>
-	</>
-  )
-}
+    <>
+      <h1 className="hGallery">Galería</h1>
+      <div className="gallery-container">{SliderData.map(renderGallery)}</div>
+    </>
+  );
+};
