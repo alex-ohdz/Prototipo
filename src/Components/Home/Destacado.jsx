604c5ae40eeb2b26f6f7ef037387d0505710c27d
Starcout <epreyesuclv@gmail.com>
2022-12-05 23:25:49 -0500

    add databse

--- src/Components/Home/Destacado.jsx
+++ src/Components/Home/Destacado.jsx
@@ -1,19 +1,29 @@
-import Card from "react-bootstrap/Card";
-import Col from "react-bootstrap/Col";
-import Row from "react-bootstrap/Row";
-import { cardInfo } from "../../assets/Data/data";
+import { useEffect } from 'react'
+import Card from 'react-bootstrap/Card'
+import Col from 'react-bootstrap/Col'
+import Row from 'react-bootstrap/Row'
+import { cardInfo } from '../../assets/Data/data'
 
+import {
+  fetchCards
+} from '../../fetch/firebase/controllers/cards.mjs'
 export const Destacado = () => {
+  useEffect(() => {
+    fetchCards()
+      .then(cards => {
+        console.log(cards)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+    return () => {}
+  })
   const renderCard = (card, index) => {
     return (
       <>
         <Col>
-          <Card
-            className="cartas"
-            data-aos="zoom-out-up"
-            key={index}
-          >
-            <Card.Img className="lazyload" variant="top" src={card.image} />
+          <Card className='cartas' data-aos='zoom-out-up' key={index}>
+            <Card.Img className='lazyload' variant='top' src={card.image} />
             <Card.Body>
               <Card.Title>{card.title}</Card.Title>
               <Card.Text>{card.text}</Card.Text>
@@ -21,14 +31,14 @@ export const Destacado = () => {
           </Card>
         </Col>
       </>
-    );
-  };
+    )
+  }
   return (
-    <div id="destacado">
+    <div id='destacado'>
       <h2>DESTACADO</h2>
-      <Row xs={1} sm={2} md={2} lg={3} className="g-4">
+      <Row xs={1} sm={2} md={2} lg={3} className='g-4'>
         {cardInfo.map(renderCard)}
       </Row>
     </div>
-  );
-};
+  )
+}
--- src/fetch/firebase/services/firebase.mjs
+++ src/fetch/firebase/services/firebase.mjs
@@ -1,9 +1,16 @@
-import { initializeApp } from 'firebase-admin/app';
+import { initializeApp } from "firebase/app";
 import { getFirestore, collection, getDocs, addDoc } from "firebase/firestore";
-import serviceAccount from '../keys/testing-e2777-firebase-adminsdk-vg8av-d43fd7f09f.json';
 
 const app = initializeApp(
-	serviceAccount
+	{
+		apiKey: "AIzaSyDJGvQ8HOUq6y5Hr_JXhJiVaR8khv8vBpg",
+		authDomain: "testing-e2777.firebaseapp.com",
+		projectId: "testing-e2777",
+		storageBucket: "testing-e2777.appspot.com",
+		messagingSenderId: "149748237235",
+		appId: "1:149748237235:web:0f573b9dd7d71c50c5c1c6",
+		measurementId: "G-H9TTLWXE2E"
+	}
 );
 
 const db = getFirestore(app);
