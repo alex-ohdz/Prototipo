3921dd7cd8237b76258bdac6b5abff3bec9a4b45
Alejandro Oramas H <99675215+alex-ohdz@users.noreply.github.com>
2022-12-06 12:59:01 -0500

    upgrade3

--- src/Components/CollectionPages/Historia.jsx
+++ src/Components/CollectionPages/Historia.jsx
@@ -1,12 +1,15 @@
-import { ImageSlider } from "./PartComponents/ImageSlider";
+import { Barrios } from './PartComponents/Barrios'
+import { ImageSlider } from './PartComponents/ImageSlider'
+
 
 export const Historia = () => {
   return (
     <>
-      <h2 className="linea">
+      <h2 className='linea'>
         <span>Historia</span>
       </h2>
       <ImageSlider />
+      <Barrios/>
     </>
-  );
-};
+  )
+}
--- src/Components/Home/Navegador.jsx
+++ src/Components/Home/Navegador.jsx
@@ -1,72 +1,72 @@
-import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
-import Offcanvas from "react-bootstrap/Offcanvas";
-import { Link } from "react-router-dom";
-import NavDropdown from "react-bootstrap/NavDropdown";
+import Container from 'react-bootstrap/Container'
+import Nav from 'react-bootstrap/Nav'
+import Navbar from 'react-bootstrap/Navbar'
+import Offcanvas from 'react-bootstrap/Offcanvas'
+import { Link } from 'react-router-dom'
+import NavDropdown from 'react-bootstrap/NavDropdown'
 
 export const Navegador = () => {
   return (
     <>
-      <Navbar collapseOnSelect key={"md"} expand={"md"}>
+      <Navbar collapseOnSelect key={'md'} expand={'md'}>
         <Container fluid>
-          <Link to="/">
+          <Link to='/'>
             <Navbar.Brand>
               {/* <img src="src\assets\Images\Iconos\person-circle.svg" alt="" /> */}
               Museo De Las Parrandas
             </Navbar.Brand>
           </Link>
-          <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${"md"}`} />
+          <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${'md'}`} />
           <Navbar.Offcanvas
-            id={`offcanvasNavbar-expand-${"md"}`}
-            aria-labelledby={`offcanvasNavbarLabel-expand-${"md"}`}
-            placement="top"
+            id={`offcanvasNavbar-expand-${'md'}`}
+            aria-labelledby={`offcanvasNavbarLabel-expand-${'md'}`}
+            placement='top'
           >
             <Offcanvas.Header closeButton>
-              <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${"md"}`}>
-                <Link to="/" className="cambiar">
-                  <img
+              <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${'md'}`}>
+                <Link to='/' className='cambiar'>
+                  {/* <img
                     src="src\assets\Images\Iconos\person-circle.svg"
                     alt=""
-                  />
+                  /> */}
                   Museo De Las Parrandas
                 </Link>
               </Offcanvas.Title>
             </Offcanvas.Header>
             <Offcanvas.Body>
-              <Nav className="cabecera container-fluid">
+              <Nav className='cabecera container-fluid'>
                 <NavDropdown
-                  className="cambiar Headers"
-                  title="Colleccion"
-                  id="nav-dropdown"
+                  className='cambiar Headers'
+                  title='Colleccion'
+                  id='nav-dropdown'
                 >
                   <NavDropdown.Item>
-                    <Link to="/arte" className="cambiar">
+                    <Link to='/arte' className='cambiar'>
                       Arte
                     </Link>
                   </NavDropdown.Item>
                   <NavDropdown.Item>
-                    <Link to="/fotografia" className="cambiar">
+                    <Link to='/fotografia' className='cambiar'>
                       Fotografia
                     </Link>
                   </NavDropdown.Item>
                   <NavDropdown.Item>
-                    <Link to="/documentos" className="cambiar">
+                    <Link to='/documentos' className='cambiar'>
                       Documentos
                     </Link>
                   </NavDropdown.Item>
                   <NavDropdown.Item>
-                    <Link to="/historia" className="cambiar">
+                    <Link to='/historia' className='cambiar'>
                       Historia
                     </Link>
                   </NavDropdown.Item>
                 </NavDropdown>
 
-                <Link to="/visita" className="cambiar Headers">
+                <Link to='/visita' className='cambiar Headers'>
                   Visita
                 </Link>
 
-                <Link to="/eventos" className="cambiar Headers">
+                <Link to='/eventos' className='cambiar Headers'>
                   Eventos
                 </Link>
               </Nav>
@@ -75,5 +75,5 @@ export const Navegador = () => {
         </Container>
       </Navbar>
     </>
-  );
-};
+  )
+}
