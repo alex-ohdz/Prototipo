3353619314e3d5f4661d76c877380e0b2909b084
Starcout <epreyesuclv@gmail.com>
2022-12-09 00:18:17 -0500

    fix add mapa image

--- src/Components/Home/HomeParts/Mapa.jsx
+++ src/Components/Home/HomeParts/Mapa.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-
+import mapaImg from '../../../assets/Images/mapa.jpg'
 export const Mapa = () => {
   return (
     <div className='mapaBox'>
       <a href='https://goo.gl/maps/4ms2WdZ52qxSohya7'>
-        <img className='mapaM' src='src\assets\Images\mapa.jpg' alt='Museo' />
+        <img className='mapaM' src={mapaImg} alt='Museo' />
       </a>
     </div>
   )
